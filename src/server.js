25e75d72a782c11761b9e48df37930b8d241d408
Leonardo <leonardosilvasantosss22@gmail.com>
2023-12-21 21:39:55 -0300

    Added GET route

--- src/server.js
+++ src/server.js
@@ -2,5 +2,9 @@ const express = require("express"); // Import
 
 const app = express(); // Initialize
 
+app.get("/message", (request, response) => {
+  response.send("Hello World!");
+});
+
 const PORT = 3333; // Define Port
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`)); // When the application starts, execute function
