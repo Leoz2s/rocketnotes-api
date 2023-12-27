d68661d61c94e68b2c59d1cb462536b7411e56f0
Leonardo <leonardosilvasantosss22@gmail.com>
2023-12-26 23:08:39 -0300

    Added controllers and error treatment

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,8 +1,35 @@
 const {Router} = require("express");
 
+const UsersController = require("../controllers/UsersController");
+
 const usersRoutes = Router();
 
-// {
+const usersController = new UsersController;
+
+usersRoutes.post("/", usersController.create);
+
+module.exports = usersRoutes;
+
+// { Examples - middleware and routes
+    // Example of middleware:
+// function myMiddleware(request, response, next) {
+//   console.log("Você passou pelo Middleware!");
+
+//   if(!request.body.isAdmin){
+//     return response.json({message: "user unauthorized"});
+//   };
+
+//   next();
+// };
+
+// // Using the middleware for all routes:
+// // usersRoutes.use(myMiddleware);
+
+// // Using for specific routes example:
+// // usersRoutes.post("/", myMiddleware, usersController.create);
+// }
+
+// { Examples of routes:
 // // usersRoutes.get("/message/:id/:user", (request, response) => {
 // //   const {id, user} = request.params
 
@@ -18,12 +45,3 @@ const usersRoutes = Router();
 // //   response.send(`Page: ${page}. Limit: ${limit}.`);
 // // });
 // }
-
-usersRoutes.post("/", (request, response) => {
-  const {name, email, password} = request.body;
-
-  // response.send(`Name: ${name}. E-mail: ${email}. Password: ${password}.`);
-  response.json({name, email, password});
-});
-
-module.exports = usersRoutes;
