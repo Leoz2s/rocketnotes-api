30abdc82697ad7d9efb054ddfd133db5334de9d5
Leonardo <leonardosilvasantosss22@gmail.com>
2024-01-07 22:02:42 -0300

    Added password cryptography and PUT route

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,15 +1,64 @@
+const {hash} = require("bcryptjs");
 const AppError = require("../utils/AppError");
+const sqliteConnection = require("../database/sqlite");
 
 class UsersController {
-  create(request, response) {
+  async create(request, response) {
     const {name, email, password} = request.body;
 
-    if(!name) {
-      throw new AppError("Name is obligatory.");
-    }
+    const database = await sqliteConnection();
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
 
-    // response.send(`Name: ${name}. E-mail: ${email}. Password: ${password}.`);
-    response.status(201).json({name, email, password});
+    if(checkUserExists){
+      throw new AppError("Este e-mail já está em uso.")
+    };
+
+    const hashedPassword = await hash(password, 8);
+
+    await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword]);
+
+    return response.status(201).json();
+
+    // if(!name) {
+    //   throw new AppError("Name is obligatory.");
+    // }
+
+    // // response.send(`Name: ${name}. E-mail: ${email}. Password: ${password}.`);
+    // response.status(201).json({name, email, password});
+  };
+
+  async update(request, response) {
+    const {name, email, password} = request.body;
+    const {id} = request.params;
+
+    const database = await sqliteConnection();
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+    if(!user) {
+      throw new AppError("User not found.");
+    };
+
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("This e-mail is in use.");
+    }else if(userWithUpdatedEmail && userWithUpdatedEmail.id == user.id){
+      throw new AppError("This e-mail is already your current e-mail.");
+    };
+
+    user.name = name;
+    user.email = email;
+
+    await database.run(`
+      UPDATE users SET 
+      name = ?,
+      email = ?,
+      updated_at = ?
+      WHERE id = ?`,
+      [user.name, user.email, new Date(), user.id]
+    );
+
+    return response.status(200).json();
   };
 };
 
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -8,6 +8,8 @@ const usersController = new UsersController;
 
 usersRoutes.post("/", usersController.create);
 
+usersRoutes.put("/:id", usersController.update);
+
 module.exports = usersRoutes;
 
 // { Examples - middleware and routes
