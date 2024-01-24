f300173b70ba7eefa344620d3b11984d2db0606c
Leonardo <leonardosilvasantosss22@gmail.com>
2024-01-24 18:06:05 -0300

    Added Notes route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const {Router} = require("express");
 
 const usersRoutes = require("./users.routes");
+const notesRoutes = require("./notes.routes");
 
 const routes = Router();
 
-routes.use("/users", usersRoutes);
 // routes.use("rota", (request, response) => {}); // Agora quando a rota for usada, vai ser redirecionada para userRoutes.
+routes.use("/users", usersRoutes);
+routes.use("/notes", notesRoutes);
 
 module.exports = routes; // Exportando. Todas as rotas da aplicação estão nele.
