c792e067b50b918eaf4fbea4f55f2acb6ea81b28
Leonardo <leonardosilvasantosss22@gmail.com>
2024-01-30 21:19:36 -0300

    Added tags route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const {Router} = require("express");
 
 const usersRoutes = require("./users.routes");
 const notesRoutes = require("./notes.routes");
+const tagsRoutes = require("./tags.routes");
 
 const routes = Router();
 
 // routes.use("rota", (request, response) => {}); // Agora quando a rota for usada, vai ser redirecionada para userRoutes.
 routes.use("/users", usersRoutes);
 routes.use("/notes", notesRoutes);
+routes.use("/tags", tagsRoutes);
 
 module.exports = routes; // Exportando. Todas as rotas da aplicação estão nele.
