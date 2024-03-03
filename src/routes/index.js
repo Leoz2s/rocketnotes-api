dca589f36bd8e9b0d3c59a84c311bf92f3840ddf
Leonardo <leonardosilvasantosss22@gmail.com>
2024-03-02 21:00:53 -0300

    Sessions route and controller

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const {Router} = require("express");
 
 const usersRoutes = require("./users.routes");
+const sessionsRoutes = require("./sessions.routes");
 const notesRoutes = require("./notes.routes");
 const tagsRoutes = require("./tags.routes");
 
@@ -8,7 +9,8 @@ const routes = Router();
 
 // routes.use("rota", (request, response) => {}); // Agora quando a rota for usada, vai ser redirecionada para userRoutes.
 routes.use("/users", usersRoutes);
+routes.use("/sessions", sessionsRoutes);
 routes.use("/notes", notesRoutes);
 routes.use("/tags", tagsRoutes);
 
-module.exports = routes; // Exportando. Todas as rotas da aplicação estão nele.
+module.exports = routes; // Exportando todas as rotas da aplicação.
