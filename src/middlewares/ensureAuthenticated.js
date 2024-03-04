8daa40d40d27e1c74d04f0879b6bc210720925c5
Leonardo <leonardosilvasantosss22@gmail.com>
2024-03-03 21:26:12 -0300

    Authetication tokens added

---
+++ src/middlewares/ensureAuthenticated.js
@@ -0,0 +1,27 @@
+const AppError = require("../utils/AppError");
+const {verify} = require("jsonwebtoken");
+const authConfig = require("../configs/auth");
+
+function ensureAuthenticated(request, response, next) {
+  const authHeader = request.headers.authorization;
+
+  if(!authHeader) {
+    throw new AppError("JWT token not informed", 401);
+  };
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    verify(token, authConfig.jwt.secret);
+
+    request.user = {
+      id: Number(user_id),
+    };
+
+    return next();
+  } catch {
+    throw new AppError("Invalid JWT token", 401);
+  };
+};
+
+module.exports = ensureAuthenticated;
