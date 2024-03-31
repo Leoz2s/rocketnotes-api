174ce385e92c85aa3a4390017566e789719def62
Leonardo <leonardosilvasantosss22@gmail.com>
2024-03-30 23:31:10 -0300

    Dependence inversion

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,28 +1,20 @@
-const {hash, compare} = require("bcryptjs");
 const AppError = require("../utils/AppError");
+const {hash, compare} = require("bcryptjs");
+
 const sqliteConnection = require("../database/sqlite");
+
+const UserRepository = require("../repositories/UserRepository");
+const UserCreateService = require("../services/UserCreateService");
 
 class UsersController {
   async create(request, response) {
     const {name, email, password} = request.body;
 
-    const database = await sqliteConnection();
-    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+    const userRepository = new UserRepository();
+    const userCreateService = new UserCreateService(userRepository);
+    await userCreateService.execute({name, email, password});
 
-    if(checkUserExists){
-      throw new AppError("This e-mail is already in use.")
-    };
-
-    const hashedPassword = await hash(password, 8);
-
-    await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword]);
-
-    return response.status(201).json();
-
-    // if(!name) {
-    //   throw new AppError("Name is obligatory.");
-    // }
-
+    return response.status(201).json("User created.");
     // // response.send(`Name: ${name}. E-mail: ${email}. Password: ${password}.`);
     // response.status(201).json({name, email, password});
   };
---
+++ src/services/UserCreateService.js
@@ -0,0 +1,27 @@
+const AppError = require("../utils/AppError");
+const {hash} = require("bcryptjs");
+
+class UserCreateService {
+  constructor(userRepository) {
+    this.userRepository = userRepository;
+  };
+  
+  async execute({name, email, password}) {
+    // const userRepository = new UserRepository; // Explicit dependence
+
+    const checkUserExists = await this.userRepository.findByEmail(email);
+    // const database = await sqliteConnection();
+    // const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+    if(checkUserExists){
+      throw new AppError("This e-mail is already in use.")
+    };
+
+    const hashedPassword = await hash(password, 8);
+
+    await this.userRepository.create({name, email, password: hashedPassword});
+    // await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword]);
+  };
+};
+
+module.exports = UserCreateService;
